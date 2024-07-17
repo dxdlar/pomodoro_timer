95b8d24d2874a15c51271e544e71de81317affb2
dxdlar <dxdlar@gmail.com>
2024-07-17 15:11:11 +0900

    Timer 화면과 Settings 화면 전환 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Timer from "./Timer";
-import { useState } from "react";
+import Settings from "./Settings";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [showSettings, setShowSettings] = useState(false);
+
   const [isPaused] = useState();
 
   return (
-    <main>
-      <Timer isPaused={isPaused} />
-    </main>
+    <main>{showSettings ? <Settings /> : <Timer isPaused={isPaused} />}</main>
   );
 }
 
