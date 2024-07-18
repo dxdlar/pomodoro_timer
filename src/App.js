0b073fe6c54271280b55d234a12d7c6ae0268402
dxdlar <dxdlar@gmail.com>
2024-07-18 14:37:49 +0900

    useContext provider 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "./App.css";
 import Timer from "./Timer";
 import Settings from "./Settings";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import SettingsContext from "./SettingsContext";
 
 function App() {
   const [showSettings, setShowSettings] = useState(false);
-
-  const [isPaused] = useState();
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
 
   return (
-    <main>{showSettings ? <Settings /> : <Timer isPaused={isPaused} />}</main>
+    <main>
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          setWorkMinutes,
+          breakMinutes,
+          setBreakMinutes,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
   );
 }
 
